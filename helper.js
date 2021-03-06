23c7ff460bb6c44c9d14a9a8829ac337a44ce7eb
Joshua Dean <joshualukedean@gmail.com>
2021-03-06 12:41:34 +0000

    Updated functions

--- helper.js
+++ helper.js
@@ -1,7 +1,7 @@
 
 
 /**
- * Returns x raised to the n-th power.
+ * Returns WebEx constructed string.
  *
  * @param {string} ctx The type of transfer.
  * @param {object} body The metadata of the starling transfer.
@@ -17,8 +17,16 @@ let generateMessageInterface = (ctx, body) => {
     switch(ctx) {
         case "transferOut" :
             returnedMessage = generateTransferOutMessage(body);
+            break;
         case "transferIn" :
             returnedMessage = generateTransferInMessage(body);
+            break;
+        case "standingOrder" :
+            returnedMessage = generateStandingOrderMessage(body);
+            break;
+        default:
+            console.log('LOG: No Suitable Paymeny Context')
+            break;
     }
 
     return returnedMessage;
@@ -38,7 +46,7 @@ let generateTransferOutMessage = (body) => {
     generatedString += "# 💸 Outbound Payment Alert! 💸 \n"
     generatedString += "--- \n"
     generatedString += "A Faster Payment of £" + (body.content.amount)*-1 +" has just been sent to **_" + body.content.counterParty + "_**. \n"
-    generatedString += "Payment Reference: \n"
+    generatedString += "### Payment Reference: \n"
     generatedString += "``` \n" + body.content.reference + "\n ```"
 
     return generatedString
@@ -58,10 +66,31 @@ let generateTransferOutMessage = (body) => {
     generatedString += "# 💸 Inbound Payment Alert! 💸 \n"
     generatedString += "--- \n"
     generatedString += "A Faster Payment of £" + (body.content.amount)*-1 +" has just been recieved by **_" + body.content.counterParty + "_**. \n"
-    generatedString += "Payment Reference: \n"
+    generatedString += "### Payment Reference: \n"
     generatedString += "``` \n" + body.content.reference + "\n ```"
 
     return generatedString
 }
+
+/**
+ * Returns x raised to the n-th power.
+ *
+ * @param {string} body The metadata of the starling transfer.
+ * @return {string}  Constructed string with parameters.
+ */
+ let generateStandingOrderMessage = (body) => {
+    // String to procederally generate
+    let generatedString = ""
+
+    // Append Introduction 
+    generatedString += "# 💸 Standing Order Payment Alert! 💸 \n"
+    generatedString += "--- \n"
+    generatedString += "A Faster Payment of £" + (body.content.amount)*-1 +" has just been recieved by **_" + body.content.counterParty + "_**. \n"
+    generatedString += "### Payment Reference: \n"
+    generatedString += "``` \n" + body.content.reference + "\n ```"
+
+    return generatedString
+}
+
 
 exports.generateMessageInterface = generateMessageInterface;
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+// Import libraries
 const express = require('express')
 const Webex = require('webex');
 const bodyParser = require('body-parser');
+const log4js = require("log4js");
 const helper = require('./helper');
+
+// Instantiate Libraries
 const app = express()
-const port = process.env.PORT || 8080;
-
-
+const logger = log4js.getLogger();
 require('dotenv').config()
 
-let targetUser = 'joshudea@cisco.com'
-
-console.log(process.env.BOT_KEY)
+// Set up settings
+const port = process.env.PORT || 8080;
+const targetUser = 'joshudea@cisco.com'
 
 const webex = Webex.init({
     credentials: {
@@ -34,8 +36,26 @@ app.post('/sendMoney', (req, res) => {
     res.sendStatus(200)
   })
 
+  //endpoint for money being sent 
+app.post('/recieveMoney', (req, res) => {
+    webexMessage = helper.generateMessageInterface("transferIn", req.body)
+
+    webex.messages.create({
+        markdown: webexMessage,
+        toPersonEmail: targetUser
+    })
+
+    res.sendStatus(200)
+  })
+
 //
 app.post('/sendStanding', (req, res) => {
+    webexMessage = helper.generateMessageInterface("standingOrder", req.body)
+
+    webex.messages.create({
+        markdown: webexMessage,
+        toPersonEmail: targetUser
+    })
 
     res.sendStatus(200)
 })
